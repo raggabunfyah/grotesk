b9b9c41ff180738cc38123e899917e91265bc393
raggabunfyah <depressionintekirdag@gmail.com>
2026-03-23 22:18:33 +0300

    Enable static export for Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // skip strict mode
+  output: "export",
   reactStrictMode: false,
   env: {
     googleAnalyticsId: process.env.NODE_ENV === "production" ? process.env.GA_MEASUREMENT_ID : "",
